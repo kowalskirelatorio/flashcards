c80dbe07d3c14a17b6cd74baa0b7e8ef45e825f4
kowalsi <106552335+kowalskirelatorio@users.noreply.github.com>
2024-09-18 07:41:45 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,99 @@
-
+criaCartao (
+    'História',
+    'Quais países foram os principais na guerra fria?',
+    'Eua e União Soviética')
+    
+    criaCartao (
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'Oceano pacífico')
+    
+    criaCartao (
+    'História',
+    'Em que ano acabou a 2° guerra mundial?',
+    '1945')
+    
+    criaCartao (
+    'História',
+    'Qual foi a data do início da revolução francesa?',
+    '5 de maio de 1789')
+    
+    criaCartao (
+    'Química',
+    'Qual o nome do elemento químico de fórmula C6H6?',
+    'Benzeno')
+    
+    criaCartao (
+    'Física',
+    'Qual matriz energética é muito usado no Brasil?',
+    'Energia hidrelétrica')
+    
+    criaCartao (
+    'Filosofia',
+    'De qual filósofo é a frase a seguir: "Penso, Logo existo"?',
+    'René Descartes')
+    
+    criaCartao (
+    'História',
+    'Qual país saiu derrotado da guerra do Paraguai?',
+    'Paraguai')
+    
+    criaCartao (
+    'Matemática',
+    'Qual é o valor do seno de 30°?',
+    '1/2')
+    
+    criaCartao (
+    'Filosofia',
+    'O período da história grega e o filósofo que disse a frase: "Só sei que nada sei", foram, respectivamente :',
+    'Período clássico e Sócrates')
+    
+    criaCartao (
+    'Biologia',
+    'Quais são duas das principais características dos mamíferos?',
+    'Glândulas mamárias e as presenças de pelos')
+    
+    criaCartao (
+    'Física',
+    'Qual a função de um resistor?',
+    'Limitar o fluxo de carga elétrica por meio da conversão da energia elétrica em energia térmica’)
+    
+    criaCartao (
+    'História',
+    'Qual foi a data da proclamação da república do Brasil?',
+    '15 de Novembro de 1889')
+    
+    criaCartao (
+    'Matemática',
+    'Qual a fórmula para se calcular a, hipotenusa?',
+    'a² + b² = c²')
+    
+    criaCartao (
+    'Programação',
+    'O que faz uma função em um código?',
+    'É um bloco de código que executa alguma operação')
+    
+    criaCartao (
+    'Química',
+    'Qual elemento química é representado por Pt?',
+    'Platina')
+    
+    criaCartao (
+    'Matemática',
+    'Qual a fórmula para se calcular a área de um triângulo equilátero?',
+    'A = L² √3 / 4')
+    
+    criaCartao (
+    'História',
+    'Que ano ocorreu a queda do muro de Berlim?',
+    '1989')
+    
+    criaCartao (
+    'História',
+    'Onde aconteceu a guerra do golfo?',
+    'Oriente médio')
+    
+    criaCartao (
+    'História',
+    'Qual presidente brasileiro assumiu o cargo de presidente após a ditadura militar?',
+    'José Sarney')
